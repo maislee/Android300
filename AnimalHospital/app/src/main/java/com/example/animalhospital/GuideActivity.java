c6662bdd8f41456f01c6460b301996203b9182df
miaslee <sub200512@gmail.com>
2023-05-16 16:09:37 +0900

    0516

---
+++ AnimalHospital/app/src/main/java/com/example/animalhospital/GuideActivity.java
@@ -0,0 +1,36 @@
+package com.example.animalhospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+        findViewById(R.id.img_map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=서울로봇고"));
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_pc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"));
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
